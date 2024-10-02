47eba43fee12f81ed7aba56080aeb50e1bc73a7b
adams <dvalekseev2007@gmail.com>
2024-10-02 19:41:12 +0300

    Добавил интерфейс общения с фронтом

--- service/Service.h
+++ service/Service.h
@@ -5,14 +5,17 @@
 #ifndef COURIER_SERVICE_MODELING_SERVICE_H
 #define COURIER_SERVICE_MODELING_SERVICE_H
 
+#include "../Courier/Courier.h"
+#include "vector"
 
 class Service {
-public:
-    void nextStep();
+ public:
+  Service(int branch_counter, int courier_counter, std::vector<int> couriers_start_position);
+  std::vector<Courier *> getCouriers();
+  void nextStep();
 
-private:
+ private:
 
 };
-
 
 #endif //COURIER_SERVICE_MODELING_SERVICE_H
