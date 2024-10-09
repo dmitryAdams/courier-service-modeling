a596921d548c6dc7ed85c0384d3ffdca69660b1d
Cirulnichek <tegor247@gmail.com>
2024-10-09 10:50:45 +0300

    Debugged backend and Generator

--- Courier/Courier.h
+++ Courier/Courier.h
@@ -12,35 +12,35 @@
 #include "../Branch//Branch.h"
 
 class Courier {
- public:
-  Courier(const std::vector<std::vector<std::pair<int, int>>>& dist, int cur, int id)
-      : dist_(dist), cur_(cur), id_(id), timeForFree_(0), timeToNext_(0) {}
+public:
+    Courier(const std::vector<std::vector<std::pair<int, int>>> &dist, int cur, int id)
+            : dist_(dist), cur_(cur), id_(id), timeForFree_(0), timeToNext_(0) {}
 
-  int getId() const;
+    int getId() const;
 
-  bool isOnTheWay() const;
+    bool isOnTheWay() const;
 
-  void setWay(int from, int to);
+    void setWay(int from, int to);
 
-  int comingFrom() const;
+    int comingFrom() const;
 
-  int goingTo() const;
+    int goingTo() const;
 
-  int timeForFree() const;
+    int timeForFree() const;
 
-  int getLast() const;
+    int getLast() const;
 
-  std::string remainingTravelTime() const;
+    std::string remainingTravelTime() const;
 
-  void next(int step);
+    void next(int step);
 
- private:
-  int id_;
-  int cur_ = -1;
-  int timeToNext_;
-  int timeForFree_;
-  const std::vector<std::vector<std::pair<int, int>>>& dist_;
-  std::queue<int> targets_;
+private:
+    int id_;
+    int cur_ = -1;
+    int timeToNext_;
+    int timeForFree_;
+    const std::vector<std::vector<std::pair<int, int>>> &dist_;
+    std::queue<int> targets_;
 };
 
 #endif  // COURIER_SERVICE_MODELING_COURIER_H
